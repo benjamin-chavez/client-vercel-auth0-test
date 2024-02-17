285f5f5bf7e4652964141648c8d1f169c4681b85
Ben Chavez <ben.m.chavez@gmail.com>
2024-02-17 02:08:13 -0600

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,43 +44,20 @@ export default withMiddlewareAuthRequired(async function middleware(
   //   // const token = user?.accessToken;
   //   // requestHeaders.set('x-hello-from-middleware1', `hello-Bearer ${token}`);
   //   // requestHeaders.set('Authorization', `Bearer ${token}`);
-  // /
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('Authorization', `Bearer - you wish`);
-  // const response = NextResponse.next();
-  // const user = await getSession(request, response);
-  // const token = user?.accessToken;
-
-  // const resStr = JSON.stringify(response, null, 2);
-  // const reqStr = JSON.stringify(request, null, 2);
-  // Setting custom headers
-  const token = '1';
-  // if (token) {
-  requestHeaders.set('x-hello-from-middleware1', `hello-Bearer ${token}`);
-  requestHeaders.set('Authorization', `Bearer ${token}`);
-  // }
-
-  // Now, use requestHeaders for any fetch call to your server
-  // Example: fetch('your-server-endpoint', { headers: requestHeaders });
-
-  // Return a response - Note that this does not modify the original request headers
-  return NextResponse.next();
+  requestHeaders.set('x-hello-from-middleware1', 'hello');
 
   // You can also set request headers in NextResponse.rewrite
-  // const response = NextResponse.next({
-  //   request: {
-  //     // New request headers
-  //     headers: requestHeaders,
-  //   },
-  // });
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
 
-  // const user = await getSession(request, response);
-  // const token = user?.accessToken;
-  // // Set a new response header `x-hello-from-middleware2`
-  // response.headers.set('x-hello-from-middleware2', 'hello');
-  // response.headers.set('Authorization', `Bearer ${token}`);
-  // // response.headers.set('path', `${req.nextUrl.pathname}`);
-  // return response;
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set('x-hello-from-middleware2', 'hello');
+  return response;
 });
 
 export const config = {
