2523ac918998fb105658f536b30804ad8df1d409
Ben Chavez <ben.m.chavez@gmail.com>
2024-02-17 01:38:27 -0600

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,7 +30,14 @@ export default withMiddlewareAuthRequired(async function middleware(
   // response.headers.set('path', `${req.nextUrl.pathname}`);
 
   // return response;
+  const session = await getSession();
+  const user = session ? session.user : null;
+
   const requestHeaders = new Headers(request.headers);
+  // const user = await getSession(request, response);
+  const token = user?.accessToken;
+  requestHeaders.set('x-hello-from-middleware1', 'hello');
+  request.headers.set('Authorization', `Bearer ${token}`);
 
   // You can also set request headers in NextResponse.rewrite
   const response = NextResponse.next({
@@ -40,10 +47,8 @@ export default withMiddlewareAuthRequired(async function middleware(
     },
   });
 
-  const user = await getSession(request, response);
-  const token = user?.accessToken;
-  requestHeaders.set('x-hello-from-middleware1', 'hello');
-  request.headers.set('Authorization', `Bearer ${token}`);
+  // const user = await getSession(request, response);
+  // const token = user?.accessToken;
   // Set a new response header `x-hello-from-middleware2`
   response.headers.set('x-hello-from-middleware2', 'hello');
   response.headers.set('Authorization', `Bearer ${token}`);
