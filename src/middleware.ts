40ca40bafa1d0f1ab5a1f3d7992dda8df2e6bf1c
Ben Chavez <ben.m.chavez@gmail.com>
2024-02-17 01:29:18 -0600

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,34 +7,49 @@ import {
 import { NextRequest, NextResponse } from 'next/server';
 
 export default withMiddlewareAuthRequired(async function middleware(
-  req: NextRequest
+  request: NextRequest
 ) {
-  if (req.nextUrl.pathname.startsWith('/api/auth')) {
-    return;
-  }
-  if (req.nextUrl.pathname.startsWith('/private')) {
-    return;
-  }
+  // if (req.nextUrl.pathname.startsWith('/api/auth')) {
+  //   return;
+  // }
+  // if (req.nextUrl.pathname.startsWith('/private')) {
+  //   return;
+  // }
 
-  if (req.nextUrl.pathname.startsWith('/api/private-route')) {
-    return;
-  }
+  // if (req.nextUrl.pathname.startsWith('/api/private-route')) {
+  //   return;
+  // }
 
+  // const response = NextResponse.next({
+  //   request: {
+  //     headers: new Headers(req.headers),
+  //   },
+  // });
+
+  // const user = await getSession(req, response);
+  // const token = user?.accessToken;
+
+  // response.headers.set('Authorization', `Bearer ${token}`);
+  // response.headers.set('path', `${req.nextUrl.pathname}`);
+
+  // return response;
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-hello-from-middleware1', 'hello');
+
+  // You can also set request headers in NextResponse.rewrite
   const response = NextResponse.next({
     request: {
-      headers: new Headers(req.headers),
+      // New request headers
+      headers: requestHeaders,
     },
   });
 
-  const user = await getSession(req, response);
-  const token = user?.accessToken;
-
-  response.headers.set('Authorization', `Bearer ${token}`);
-  response.headers.set('path', `${req.nextUrl.pathname}`);
-
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set('x-hello-from-middleware2', 'hello');
   return response;
 });
 
 export const config = {
-  matcher: ['/private/:path*', '/api/:path*'],
+  // matcher: ['/private/:path*', '/api/:path*'],
+  matcher: ['*'],
 };
